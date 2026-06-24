11f50d23336ed5bfad9ba096631432fd9a369e0d
Jiovanni <Jkitlo2005@gmail.com>
2026-06-24 09:37:29 +0300

    refactor: migrate GET / endpoints  to PostgreSQL

--- src/controllers/todo.controller.ts
+++ src/controllers/todo.controller.ts
@@ -2,8 +2,21 @@ import { Request , Response } from "express";
 import { todos } from "../data/todo.js"; // will remove this after i finish all the endpoints
 import { db } from "../config/db.js";
 
-export const getTodos = ( req: Request, res: Response ) => {
-    res.json( todos );
+export const getTodos =  async ( req: Request, res: Response ) => {
+
+    try{
+        const result = await db.query(
+            "SELECT * from todos "
+        );
+        res.status(200).json(result.rows);// because result contains meta data + actual data
+
+    }
+    catch( error ){
+        res.status(500).json({
+            message : "Error displaying todos."
+        })
+    }
+    
 }
 
 export const createTodos = async ( req: Request, res: Response ) => {
@@ -14,8 +27,8 @@ try {
         "INSERT INTO todos (title , completed) VALUES ($1,$2) RETURNING *", [title,false] );
         // the $1, $2 are only placeholders, because SQL does not see the Javascript variables
         res.status(201).json(result.rows[0]);
-        //because database does not return a clean object, it returns a result wrapper and i choose what part is the 
-        // response, and the RETURNING * returns the inserted rows as an array, and since i only insert one at a time 
+        //because database does not return the inserted object directly, it returns a result wrapper and i choose what part is the 
+        // response, and the RETURNING * returns the rows affected by the query as an array, and since i only insert one at a time 
         // so i always return the row at index 0.
     }
     catch ( error ){
@@ -28,16 +41,28 @@ try {
 
 }
 
-export const getTodoById = ( req: Request , res : Response ) => {
-    const id = Number ( req . params . id );
-    const todo = todos.find( td => td.id === id );
-    if( !todo) {
-        res.status(404).json({
-            message : "Todo Not Found :( "
-        });
+export const getTodoById =  async ( req: Request , res : Response ) => {
+
+    try {
+        const id = Number(req.params.id);
+        const result = await db.query(
+            "SELECT * from todos WHERE id=$1", [id]
+        );
+        
+        if ( result.rows.length === 0 ){
+            res.status(404).json({
+                message : "Todo Not Found :("
+            });
+        }
+        res.status(200).json(result.rows);
+    }
+    catch( error ){
+        res.status(500).json({
+            message : "Error displaying todo."
+        })
     }
 
-    res.status(200).json(todo);
+    
 }
 
 export const updateTodo = ( req : Request, res: Response ) => {
